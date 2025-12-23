8ff3530e946da5cf64a04d736b737940be28075b
Yash Gadgil <102785510+yash-gadgil@users.noreply.github.com>
2025-12-23 22:56:57 +0530

    Minor QuestsScreen changes

--- src/main/java/com/jackydoge/lorebound/gui/QuestsScreen.java
+++ src/main/java/com/jackydoge/lorebound/gui/QuestsScreen.java
@@ -1,9 +1,13 @@
 package com.jackydoge.lorebound.gui;
 
 import com.jackydoge.lorebound.gui.components.QuestTab;
+import com.jackydoge.lorebound.util.ModKeyBinds;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.layouts.HeaderAndFooterLayout;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
@@ -31,14 +35,29 @@ public class QuestsScreen extends Screen {
     @Override
     protected void init() {
         this.layout.addTitleHeader(TITLE, this.font);
-        super.init();
+        this.layout.addToFooter(Button.builder(CommonComponents.GUI_DONE, p_329618_ -> this.onClose()).width(200).build());
+        this.layout.arrangeElements();
     }
 
     @Override
     public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
+
+        RenderSystem.enableBlend();
+
         //pGuiGraphics.fill((this.width - GUI_WIDTH) / 2, (this.height - GUI_HEIGHT) / 2,  (this.width + GUI_WIDTH) / 2 , (this.height + GUI_HEIGHT) / 2, 0xFF444444);
         this.renderBackground(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
         super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+    }
+    @Override
+    public boolean keyPressed(int pKeyCode, int pScanCode, int pModifiers) {
+        if (ModKeyBinds.openQuests.matches(pKeyCode, pScanCode)) {
+            assert this.minecraft != null;
+            this.minecraft.setScreen(null);
+            this.minecraft.mouseHandler.grabMouse();
+            return true;
+        } else {
+            return super.keyPressed(pKeyCode, pScanCode, pModifiers);
+        }
     }
 
     @Override
